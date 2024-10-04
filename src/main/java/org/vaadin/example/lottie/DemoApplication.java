26a39abb47333e5c1ec57a7270575354064f0a37
Sami Ekblad <sami@vaadin.com>
2024-10-04 12:13:12 +0300

    AppShellConfigurator

--- src/main/java/org/vaadin/example/lottie/DemoApplication.java
+++ src/main/java/org/vaadin/example/lottie/DemoApplication.java
@@ -1,12 +1,13 @@
 package org.vaadin.example.lottie;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Push
-public class DemoApplication {
+public class DemoApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
