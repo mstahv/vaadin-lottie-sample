6e0eb52dd8a162edde80016a943bc3e5b0c61d7f
Sami Ekblad <sami@vaadin.com>
2024-10-04 12:01:50 +0300

    Enable push

--- src/main/java/org/vaadin/example/lottie/DemoApplication.java
+++ src/main/java/org/vaadin/example/lottie/DemoApplication.java
@@ -1,9 +1,11 @@
 package org.vaadin.example.lottie;
 
+import com.vaadin.flow.component.page.Push;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Push
 public class DemoApplication {
 
 	public static void main(String[] args) {
